a5ad4c45029e876510f3d73176275e4551114a79
KennyLee <kennylee26@gmail.com>
2015-07-14 23:17:55 +0800

    简化run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /.tomcat_admin_created ] && [ -f /create_tomcat_admin_user.sh ]; then
-    /create_tomcat_admin_user.sh
-fi
-
 /bin/bash -c "${CATALINA_HOME}/bin/startup.sh && /usr/sbin/sshd -D"
 
 
